40442c5326fea1b669b0054c55d81f2a0f1be87e
João Lucas Simões Moreira <JNDGitHub@users.noreply.github.com>
2020-09-20 20:49:48 -0300

    Backend
    Mudança na verificação da senha, agora com esta verificação de fato acontecendo (hehehe)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
-from werkzeug.security import check_password_hash
+#from werkzeug.security import check_password_hash, generate_password_hash
 from functools import wraps
 from flask_sqlalchemy import sqlalchemy, SQLAlchemy
 from sqlalchemy import create_engine
@@ -16,9 +16,10 @@ app.config.update({
 db.init_app(app)
 
 class users(db.Model):
+    username = db.Column(db.String(100))
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
-    password = db.Column(db.String(80))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
 
 def login_required(f):
     @wraps(f)
@@ -43,12 +44,13 @@ def welcome():
 def login():
     error = None
     if request.method == 'POST':
+        inputEmail = request.form.get('email')
         inputUsername = request.form.get('username')
         inputPassword = request.form.get('password')
 
-        login = users.query.filter_by(username = inputUsername).first()
-        if not users:
-            error = 'Dados invalidos, tente novamente.'
+        login = users.query.filter_by(email=inputEmail).first()
+        if not login.password == inputPassword:
+            error = 'Senha invalida, tente novamente.'
         else:
             session['logged_in'] = True
             flash('Voce logou na pagina!')
@@ -67,15 +69,22 @@ def logout():
 def register():
     error = None
     defaultUsername = 'username-doido'
+    defaulEmail = 'doido@email.com'
     defaultPassword = 'senha-doida'
     if request.method == 'POST':
         inputUsername = request.form.get('username', defaultUsername)
+        inputEmail = request.form.get('email',defaulEmail)
         inputPassword = request.form.get('password', defaultPassword)
         inputRepassword = request.form.get('repassword', defaultPassword)
+
+        usuario = users.query.filter_by(email=inputEmail).first()
+        if usuario:
+            error = 'Email ja cadastrado!'
+            return render_template('register.html', error=error)
         if inputRepassword != inputPassword:
             error = 'As senhas nao coincidem.'
         else: 
-            register = users(username = inputUsername, password = inputPassword)
+            register = users(username = inputUsername, email = inputEmail, password = inputPassword)
             db.session.add(register)
             db.session.commit()
             flash('Cadastrado com sucesso!')
--- settings.py
+++ settings.py
@@ -1,11 +1,14 @@
 import sqlite3
+#from werkzeug.security import generate_password_hash
 
 banco = sqlite3.connect('bancodados.db')
 cursor = banco.cursor()
 
-#cursor.execute("CREATE TABLE users (username text,id integer primary key,password text)")
-#cursor.execute("INSERT INTO users(username, password) VALUES('JND','JNDsenha')")
-#banco.commit()
+#senha = generate_password_hash('admin', method='sha256')
+#cursor.execute("CREATE TABLE users (username text,id integer primary key,email text, password text)")
+#cursor.execute("INSERT INTO users(username, password, email) VALUES('JND','JND','JND@email.com')")
+#cursor.execute("INSERT INTO users(username, password, email) VALUES('admin','admin','admin@email.com')")
+banco.commit()
 
 cursor.execute("SELECT * FROM users")
 print(cursor.fetchall())
